c1d9b3261c76ccb8e6db8c49cf806c572401af7c
BradleyMorrison1 <70822639+BradleyMorrison1@users.noreply.github.com>
2021-02-02 09:13:14 -0400

    Initial commit

---
+++ Assets/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Vector3 moveDirection = Vector3.back;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Wall") || other.CompareTag("Enemy"))
+        {
+            Debug.Log("Hit Wall");
+
+            if (moveDirection == Vector3.back)
+            {
+                moveDirection = Vector3.forward;
+            }
+
+            else
+            {
+                moveDirection = Vector3.back;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        gameObject.transform.position += (moveDirection) * Time.deltaTime * moveSpeed;
+    }
+}
